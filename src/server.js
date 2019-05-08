9ddc7f1baab642f9082b65dd271aeab698e86a56
Jeff Pierce <piercejeffrey@gmail.com>
2019-05-07 17:04:11 -0700

    Add api server

---
+++ src/server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+const epilogue = require('epilogue')
+const OktaJwtVerifier = require('@okta/jwt-verifier')
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: '0oadejezhdB6T7rVn356',
+  issuer: 'https://dev-741623.okta.com/oauth2/default'
+})
+
+let app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+// verify JWT token middleware
+app.use((req, res, next) => {
+  // require every request to have an authorization header
+  if (!req.headers.authorization) {
+    return next(new Error('Authorization header is required'))
+  }
+  let parts = req.headers.authorization.trim().split(' ')
+  let accessToken = parts.pop()
+  oktaJwtVerifier.verifyAccessToken(accessToken)
+    .then(jwt => {
+      req.user = {
+        uid: jwt.claims.uid,
+        email: jwt.claims.sub
+      }
+      next()
+    })
+    .catch(next) // jwt did not verify!
+})
+
+// For ease of this tutorial, we are going to use SQLite to limit dependencies
+let database = new Sequelize({
+  dialect: 'sqlite',
+  storage: './test.sqlite'
+})
+
+// Define our Post model
+// id, createdAt, and updatedAt are added by sequelize automatically
+let Post = database.define('transaction', {
+  action: Sequelize.INTEGER,
+  date: Sequelize.TEXT,
+  symbol: Sequelize.TEXT,
+  shares: Sequelize.INTEGER,
+  price: Sequelize.DECIMAL
+})
+
+// Initialize epilogue
+epilogue.initialize({
+  app: app,
+  sequelize: database
+})
+
+// Create the dynamic REST resource for our Post model
+let userResource = epilogue.resource({
+  model: Transaction,
+  endpoints: ['/transactions', '/transactions/:id']
+})
+
+// Resets the database and launches the express app on :8081
+database
+  .sync({ force: true })
+  .then(() => {
+    app.listen(8081, () => {
+      console.log('listening to port localhost:8081')
+    })
+})
